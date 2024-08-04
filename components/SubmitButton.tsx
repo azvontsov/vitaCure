c9d29cc25fb31d7008cdbf5d994bec9e94407087
Anton Zvontsov <azvontsov@gmail.com>
2024-08-03 20:47:53 -0400

    feat: Add OTP verification section to Home page

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+interface ButtonProps {
+  isLoading: boolean;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      className={className ?? "shad-primary-btn w-full"}
+      disabled={isLoading}
+    >
+      {isLoading ? (
+        <div className="flex justify-center gap-4">
+          <Image
+            src="/assets/icons/loader.svg"
+            alt="loader"
+            width={24}
+            height={24}
+            className="animate-spin"
+          />
+          <span>Loading...</span>
+        </div>
+      ) : (
+        children
+      )}
+    </Button>
+  );
+};
+
+export default SubmitButton;
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,7 +1,87 @@
-import React from "react";
+"use client";
 
-const PatientForm = () => {
-  return <div>PatientForm</div>;
-};
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  SELECT = "select",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SKELETON = "skeleton",
+}
+
+export function PatientForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof UserFormValidation>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="text-24-bold">Welcome to Vita Cure</h1>
+          <p className="text-14-regular text-dark-700">
+            Please enter your username to continue.
+          </p>
+        </section>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full Name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="jdoe@mail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(202) 123-4567"
+        />
+        <SubmitButton isLoading={isLoading}>Continue</SubmitButton>
+      </form>
+    </Form>
+  );
+}
 
 export default PatientForm;
